7b4898b2352675f0ad40bac69e98312dff2f993e
Fadi Farah <farahfa@users.noreply.github.com>
2018-01-27 04:05:56 -0500

    Update DiscordWebhook.java

--- src/main/java/nz/co/jammehcow/jenkinsdiscord/DiscordWebhook.java
+++ src/main/java/nz/co/jammehcow/jenkinsdiscord/DiscordWebhook.java
@@ -123,7 +123,7 @@ class DiscordWebhook {
 
     /* Added to test things out */
     public boolean perform(AbstractBuild build, Launcher launcher, BuildListener listener) {
-        listener.getLogger().println(this.obj);
+        listener.getLogger().println((this.obj).asJson());
         return true;
     }
     /****************************/
